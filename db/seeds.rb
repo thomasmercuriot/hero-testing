a14f18ccc8bdb76685137a9ebb072a06fe7ececd
thomas mercuriot <mercuriot.thomas@gmail.com>
2024-06-25 16:22:34 +0200

    --wip-- [skip ci]

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Student.create!(name: "John Doe")
+Student.create!(name: "Jane Doe")
